07fb3dc96b8bfc634fb95bfd34e4bed2d47c9010
miyamotohk <miyamotohk@gmail.com>
2025-11-10 21:26:59 +0000

    Deploying to gh-pages from @ miyamotohk/miyamotohk.github.io@70c4799ae88973ede4174cd03556856d8b7b15f0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -53,67 +53,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{id: "projects-context-tree-based-csi-compression",
-          title: 'Context-tree-based CSI compression',
-          description: "",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/csi-compression/";
-            },},{id: "projects-fisher-rao-distance-and-applications",
-          title: 'Fisher-Rao distance and applications',
-          description: "",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/information-geometry/";
-            },},{id: "projects-spherical-codes-by-hopf-foliations",
-          title: 'Spherical codes by Hopf foliations',
-          description: "",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/spherical-codes/";
-            },},{
+          section: "News",},{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
