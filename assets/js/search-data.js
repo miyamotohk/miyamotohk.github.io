a942dc50e672db0e1d9ced2881258d67cc655e01
miyamotohk <miyamotohk@gmail.com>
2026-07-09 07:33:32 +0000

    Deploying to gh-pages from @ miyamotohk/miyamotohk.github.io@c465c75adcf5370120db2ef5cab3e1301225a69f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -42,8 +42,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-i-have-successfully-defended-my-ph-d-thesis-at-université-paris-saclay-on-12-june-2026-the-committee-was-composed-by-prof-michèle-wigger-prof-amos-lapidoth-dr-hamdi-joudeh-and-prof-albert-guillén-i-fàbregas",
-          title: 'I have successfully defended my Ph.D. thesis at Université Paris-Saclay on 12 June...',
+            },},{id: "news-i-have-successfully-defended-my-ph-d-thesis-at-université-paris-saclay-the-committee-was-composed-by-prof-michèle-wigger-prof-amos-lapidoth-dr-hamdi-joudeh-and-prof-albert-guillén-i-fàbregas",
+          title: 'I have successfully defended my Ph.D. thesis at Université Paris-Saclay. The committee was...',
           description: "",
           section: "News",},{
       id: 'light-theme',
